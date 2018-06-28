30cc778990b6e3701a5f082ce0aa95e94ae3cae9
Damien Vergnet (Darmo) <darmo117@gmail.com>
2018-06-28 22:03:08 +0200

    doc: added doc and type hints for Application class.

--- app/gui/application.py
+++ app/gui/application.py
@@ -1,32 +1,36 @@
 import os
 import sys
+import typing as typ
 
+import PyQt5.QtCore as QtC
 import PyQt5.QtGui as QtG
 import PyQt5.QtWidgets as QtW
-from PyQt5.QtCore import QThread
 
+import app.data_access as da
+import app.model as model
+import app.queries as queries
+import app.utils as utils
 import config
-from app import utils
-from app.data_access import ImageDao, TagsDao, write_playlist
 from app.logging import logger
-from app.model import Image, TagType
-from app.queries import query_to_sympy
 from .components import ImageList, TagTree
 from .dialogs import EditImageDialog, EditTagsDialog, AboutDialog
 
 
 class Application(QtW.QMainWindow):
+    """Application's main class."""
+
     def __init__(self):
         super().__init__()
 
-        self._dao = ImageDao(config.DATABASE)
-        self._tags_dao = TagsDao(config.DATABASE)
+        self._dao = da.ImageDao(config.DATABASE)
+        self._tags_dao = da.TagsDao(config.DATABASE)
 
         self._init_ui()
         utils.center(self)
 
     # noinspection PyUnresolvedReferences
     def _init_ui(self):
+        """Initializes the UI."""
         self.setWindowTitle("Image Library v" + config.VERSION)
         self.setWindowIcon(QtG.QIcon("app/icons/app_icon.png"))
         self.setGeometry(0, 0, 800, 600)
@@ -83,6 +87,7 @@ class Application(QtW.QMainWindow):
 
     # noinspection PyUnresolvedReferences
     def _init_menu(self):
+        """Initializes the menu bar."""
         menubar = self.menuBar()
 
         file_menu = menubar.addMenu("&File")
@@ -153,36 +158,42 @@ class Application(QtW.QMainWindow):
         help_menu.addAction(about_item)
 
     def _center(self):
+        """Centers the application window."""
         qr = self.frameGeometry()
         cp = QtW.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
     def _add_image(self):
+        """Opens a file chooser then add the selected image to the database."""
         file = utils.open_image_chooser(self)
         if file != utils.REJECTED:
             self._add_images([file])
 
     def _add_directory(self):
+        """Opens a file chooser then add the images from the selected directory to the database."""
         images = utils.open_directory_chooser(self)
         if images == utils.NO_IMAGES:
             utils.show_info("No images in this directory!", parent=self)
         elif images != utils.REJECTED:
             self._add_images(images)
 
-    def _add_images(self, images):
+    def _add_images(self, images: typ.List[str]):
+        """Opens the 'Add Images' dialog then adds the images to the database. Checks for duplicates."""
         if len(images) > 0:
             images_to_add = [i for i in images if not self._dao.image_registered(i)]
             if len(images_to_add) == 0:
                 text = "Image" + ("s" if len(images) > 1 else "") + " already registered!"
                 utils.show_info(text, parent=self)
             else:
-                dialog = EditImageDialog(self, show_skip=len(images_to_add) > 1, mode=EditImageDialog.ADD)
+                dialog = EditImageDialog(self, show_skip=len(images_to_add) > 1,
+                                         mode=EditImageDialog.ADD)
                 dialog.set_on_close_action(self._fetch_and_refresh)
-                dialog.set_images([Image(0, i) for i in images_to_add], {})
+                dialog.set_images([model.Image(0, i) for i in images_to_add], {})
                 dialog.show()
 
     def _replace_image(self):
+        """Opens the 'Replace Image' dialog then replaces the image with the selected one."""
         images = self._list.selected_images()
         if len(images) == 1:
             image = images[0]
@@ -195,17 +206,20 @@ class Application(QtW.QMainWindow):
             dialog.show()
 
     def _export_images(self):
+        """Opens a file saver then writes all images to a playlist file."""
         images = self._list.get_images()
         if len(images) > 0:
             file = utils.open_playlist_saver(self)
             if file != utils.REJECTED:
-                write_playlist(file, images)
+                da.write_playlist(file, images)
 
     def _fetch_and_refresh(self):
+        """Fetches images then refreshes the list."""
         self._fetch_images()
         self._refresh_tree()
 
-    def _edit_images(self, images):
+    def _edit_images(self, images: typ.List[model.Image]):
+        """Opens the 'Edit Images' dialog then updates all edited images."""
         if len(images) > 0:
             dialog = EditImageDialog(self, show_skip=len(images) > 1)
             dialog.set_on_close_action(self._fetch_and_refresh)
@@ -219,6 +233,7 @@ class Application(QtW.QMainWindow):
             dialog.show()
 
     def _delete_images(self):
+        """Deletes the selected images. User is asked to confirm the action."""
         images = self._list.selected_images()
 
         if len(images) > 0:
@@ -239,28 +254,42 @@ class Application(QtW.QMainWindow):
                 self._fetch_images()
 
     def _edit_tags(self):
+        """Opens the 'Edit Tags' dialog. Tags tree is refreshed afterwards."""
         dialog = EditTagsDialog(self)
         dialog.set_on_close_action(self._refresh_tree)
         dialog.show()
 
-    def _tree_item_clicked(self, item):
+    def _tree_item_clicked(self, item: QtW.QTreeWidgetItem):
+        """
+        Called when a item in the tags tree is clicked.
+
+        :param item: The clicked item.
+        """
         if item.whatsThis(0) == "tag":
             self._input_field.setText((self._input_field.text() + " " + item.text(0)).lstrip())
 
     def _refresh_tree(self):
-        self._tag_tree.refresh(TagType.SYMBOL_TYPES.values(), self._tags_dao.get_all_tags())
+        """Refreshes the tags tree."""
+        self._tag_tree.refresh(model.TagType.SYMBOL_TYPES.values(), self._tags_dao.get_all_tags())
 
-    class SearchThread(QThread):
-        def __init__(self, tags):
+    class _SearchThread(QtC.QThread):
+        """This thread is used to search images from a query."""
+
+        def __init__(self, query: str):
+            """
+            Creates a search thread for a query.
+
+            :param query: The query.
+            """
             super().__init__()
-            self._tags = tags
+            self._query = query
             self._images = []
             self._error = None
 
         def run(self):
-            dao = ImageDao(config.DATABASE)
+            dao = da.ImageDao(config.DATABASE)
             try:
-                expr = query_to_sympy(self._tags)
+                expr = queries.query_to_sympy(self._query)
             except ValueError as e:
                 self._error = str(e)
                 return
@@ -270,65 +299,73 @@ class Application(QtW.QMainWindow):
                 self._error = "Failed to load images!"
 
         @property
-        def fetched_images(self):
+        def fetched_images(self) -> typ.List[model.Image]:
+            """Returns all fetched images."""
             return self._images
 
-        def failed(self):
+        def failed(self) -> bool:
+            """Returns true if the operation failed."""
             return self._error is not None
 
         @property
-        def error(self):
+        def error(self) -> typ.Optional[str]:
+            """If the operation failed, returns the reason; otherwise returns None."""
             return self._error
 
     def _fetch_images(self):
+        """Fetches images matching the typed query. Starts a search thread to avoid freezing the whole application."""
         tags = self._input_field.text().strip()
         if len(tags) > 0:
             self._ok_btn.setEnabled(False)
             self._input_field.setEnabled(False)
-            self._thread = Application.SearchThread(tags)
+            self._thread = self._SearchThread(tags)
             # noinspection PyUnresolvedReferences
             self._thread.finished.connect(self._on_fetch_done)
             self._thread.start()
 
     def _on_fetch_done(self):
+        """Called when images searching is done."""
         if self._thread.failed():
             utils.show_error(self._thread.error, parent=self)
             self._ok_btn.setEnabled(True)
             self._input_field.setEnabled(True)
-            return
-        self._list.clear()
-        images = self._thread.fetched_images
-        images.sort(key=lambda i: i.path)
-        for image in images:
-            self._list.add_image(image)
-        self._ok_btn.setEnabled(True)
-        self._input_field.setEnabled(True)
-        self._input_field.setFocus()
+        else:
+            self._list.clear()
+            images = self._thread.fetched_images
+            images.sort(key=lambda i: i.path)
+            for image in images:
+                self._list.add_image(image)
+            self._ok_btn.setEnabled(True)
+            self._input_field.setEnabled(True)
+            self._input_field.setFocus()
 
     def _list_changed(self, _):
+        """Called when images list content changes."""
         self._export_item.setEnabled(self._list.count() > 0)
 
-    def _list_selection_changed(self, selection):
+    def _list_selection_changed(self, selection: list):
+        """
+        Called when the selection in images list changes.
+
+        :param selection: The current selection.
+        """
         self._replace_image_item.setEnabled(len(selection) == 1)
         self._edit_images_item.setEnabled(len(selection) > 0)
         self._delete_images_item.setEnabled(len(selection) > 0)
 
-    @staticmethod
-    def _image_to_string(image):
-        return image.path
-
     @classmethod
     def run(cls):
+        """Run an instance of this Application class."""
         try:
             app = QtW.QApplication(sys.argv)
 
             # Initialize tag types
-            tags_dao = TagsDao(config.DATABASE)
+            tags_dao = da.TagsDao(config.DATABASE)
             types = tags_dao.get_all_types()
             if types is None:
                 utils.show_error("Could not load tag types! Shutting down.")
                 sys.exit(1)
-            TagType.init(types)
+            model.TagType.init(types)
             tags_dao.close()
 
             cls().show()
